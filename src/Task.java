5c5cf51fa78d54782e2a1a49d6f6bba74989177b
ajika <52496651+karangwaajika@users.noreply.github.com>
2025-06-22 11:28:10 +0200

    feat: set task with highest priority to be added first

--- src/Task.java
+++ src/Task.java
@@ -1,7 +1,7 @@
 import java.time.Instant;
 import java.util.UUID;
 
-public class Task {
+public class Task implements Comparable<Task>{
     private UUID id;
     private String name;
     private int priority;
@@ -14,5 +14,11 @@ public class Task {
         this.priority = priority;
         this.payload = payload;
         this.createdTimestamp = Instant.now();
+    }
+
+    @Override
+    public int compareTo(Task o) {
+        //insert highest priority first.
+        return Integer.compare(o.priority, this.priority);
     }
 }
